ccaccefa68666a7ede8a8f714014850726866dca
Your Name <you@example.com>
2024-10-17 19:42:40 +0500

    Header And Rules Component Created

---
+++ src/pages/EventFlow.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../components/contributions/Event-Flow/Header";
+import Rules from "../components/contributions/Event-Flow/Rules";
+
+
+export default function EventFlow() {
+    return (
+        <>
+            <Header />
+            <Rules />
+          
+        </>
+    );
+}
