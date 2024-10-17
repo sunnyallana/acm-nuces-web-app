d3b4b4abe78a01051ba31e26578bc165ed2a1791
sunnyallana <study.sunnyallana@gmail.com>
2024-10-18 03:32:47 +0500

    Footer Details Updation: Responsiveness pending

--- src/components/GlobalComponents/FooterComponent.jsx
+++ src/components/GlobalComponents/FooterComponent.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone } from '@fortawesome/free-solid-svg-icons';
+import { faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const FooterComponent = () => {
   return (
     <footer className="bg-[#000017] text-white">
       <div className="container mx-auto flex flex-row justify-between items-start px-8 py-8 md:px-24">
         <div className="flex-1 mb-6 md:mb-0">
-          <div className="rounded bg-zinc-300 w-72 h-72 md:w-72 md:h-72 xl:w-96 xl:h-96 2xl:w-96 2xl:h-96"></div>
+          <div className="rounded bg-zinc-300 w-72 h-72 md:w-72 md:h-72 xl:w-96 xl:h-96 2xl:w-96 2xl:h-96">
+            {/* Map Embed */}
+            <iframe
+              title="Location Map"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3620.203336047934!2d67.26210887401167!3d24.856903945402394!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb3316c5276e35b%3A0x823a6a0100195ffd!2sFAST%20National%20University%20Karachi%20Campus!5e0!3m2!1sen!2s!4v1727465448702!5m2!1sen!2s"
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+            ></iframe>
+          </div>
         </div>
 
         <div className="flex-1 flex flex-col items-center md:items-between text-center md:text-left md:ml-4">
@@ -16,16 +28,21 @@ const FooterComponent = () => {
               Follow Us
             </p>
             <div className="flex space-x-4">
-  <div className="rounded bg-zinc-300 w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16"></div>
-  <div className="rounded bg-zinc-300 w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16"></div>
-  <div className="rounded bg-zinc-300 w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16"></div>
-  <div className="rounded bg-zinc-300 w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16"></div>
-</div>
+              <a href="https://www.linkedin.com/company/acmnuceskhi" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} className="text-zinc-300 w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16" />
+              </a>
+              <a href="https://www.facebook.com/acmnuceskhi/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faFacebook} className="text-zinc-300 w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16" />
+              </a>
+              <a href="https://www.instagram.com/acmnuceskhi/" target="_blank" rel="noopener noreferrer">
+                <FontAwesomeIcon icon={faInstagram} className="text-zinc-300 w-8 h-8 sm:w-10 sm:h-10 md:w-12 md:h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16" />
+              </a>
+            </div>
 
             <ul className="mt-4 space-y-1 mb-4 text-[15px] sm:text-[14px] md:text-[15px] lg:text-[16px] xl:text-[18px] 2xl:text-[25px] font-normal">
-              <li><a href="" className="hover:underline">Privacy Policy</a></li>
-              <li><a href="" className="hover:underline">Terms and Conditions</a></li>
-              <li><a href="" className="hover:underline">Code Of Conduct</a></li>
+              <li><a href="#" className="hover:underline">Privacy Policy</a></li>
+              <li><a href="#" className="hover:underline">Terms and Conditions</a></li>
+              <li><a href="#" className="hover:underline">Code Of Conduct</a></li>
             </ul>
 
             <div className="mt-8">
@@ -33,10 +50,14 @@ const FooterComponent = () => {
                 Contact Us
               </p>
               <p className="flex items-center text-[15px] sm:text-[14px] md:text-[15px] lg:text-[16px] xl:text-[18px] 2xl:text-[25px] font-normal">
-                <FontAwesomeIcon icon={faPhone} className="mr-2 text-white" /> +12-8883456789
+                <a href="tel:+9221341005416" className="flex items-center text-white">
+                  <FontAwesomeIcon icon={faPhone} className="mr-2" /> (+92-213) 410-0541-6
+                </a>
               </p>
               <p className="flex items-center text-[15px] sm:text-[14px] md:text-[15px] lg:text-[16px] xl:text-[18px] 2xl:text-[25px] font-normal">
-                <span className="mr-2">✉️</span> example@email.com
+                <a href="mailto:info@nu.edu.pk" className="flex items-center text-white">
+                  <span className="mr-2">✉️</span> info@nu.edu.pk
+                </a>
               </p>
             </div>
           </div>
--- src/pages/EventFlow.jsx
+++ src/pages/EventFlow.jsx
@@ -7,8 +7,7 @@ export default function EventFlow() {
     return (
         <>
             <Header />
-            <Rules />
-          
+            <Rules />         
         </>
     );
 }
