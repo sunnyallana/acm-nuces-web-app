ceb6a2b6974715206d8925d69912a897d4fb01af
owaisrafiq05 <owaisrafiq05@gmail.com>
2024-10-19 16:35:52 +0500

    Sponsor Component Added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,12 +1,15 @@
 import HeroSection from "../components/HomePageComponents/HeroSection"
 import AboutCodersCup from "../components/HomePageComponents/AboutCodersCup"
 import AboutACM from "../components/HomePageComponents/AboutACM"
+import SponsorComp from "../components/HomePageComponents/SponsorComp"
+
 export default function HomePage(){
     return(
         <div className="overflow-hidden">
             <HeroSection />
             <AboutCodersCup/>
             <AboutACM/>
+            <SponsorComp/>
         </div>
     )
 }
