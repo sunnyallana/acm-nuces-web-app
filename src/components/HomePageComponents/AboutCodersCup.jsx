a79af118cfe4a80681b4ef84fbd53f7c3bb2695d
Sunny Shaban Ali <66526377+sunnyallana@users.noreply.github.com>
2024-10-19 16:36:41 +0500

    minor change

--- src/components/HomePageComponents/AboutCodersCup.jsx
+++ src/components/HomePageComponents/AboutCodersCup.jsx
@@ -4,7 +4,7 @@ import logo from '../../assets/images/coders-cup-logo.png';
 export default function AboutCodersCup() {
     return (
         <div className="text-white bg-[#000017] pt-12">
-            <h1 className="bg-[#052F54] text-center text-5xl font-semibold py-6 mb-12">
+            <h1 className="bg-[#052F54] text-center text-3xl md:text-7xl font-bold py-6 px-2 mb-12">
             ABOUT <span className="text-[#17A0B7]">CODERS CUP</span>
             </h1>
             <div className="flex flex-col md:flex-row gap-8 justify-center items-center px-4">
