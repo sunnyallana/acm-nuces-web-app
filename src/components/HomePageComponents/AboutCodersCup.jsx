7609de37641d2a20fce11a38b625318bca87acb7
sunnyallana <study.sunnyallana@gmail.com>
2024-10-19 16:11:14 +0500

    Updated Wrappers Height to avoid clipping and changed color of text

--- src/components/HomePageComponents/AboutCodersCup.jsx
+++ src/components/HomePageComponents/AboutCodersCup.jsx
@@ -4,7 +4,9 @@ import logo from '../../assets/images/coders-cup-logo.png';
 export default function AboutCodersCup() {
     return (
         <div className="text-white bg-[#000017] pt-12">
-            <h1 className="bg-[#052F54] text-center text-5xl font-semibold py-6 mb-12">ABOUT CODER'S CUP</h1>
+            <h1 className="bg-[#052F54] text-center text-5xl font-semibold py-6 mb-12">
+            ABOUT <span className="text-[#17A0B7]">CODERS CUP</span>
+            </h1>
             <div className="flex flex-col md:flex-row gap-8 justify-center items-center px-4">
                 <p className='w-full md:w-6/12 text-center md:text-left'>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
