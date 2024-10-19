c27b1dd3a44722c81cb0c4944abaf6ae4b6529a5
owaisrafiq05 <owaisrafiq05@gmail.com>
2024-10-19 12:07:07 +0500

    Responsive home page done

--- src/components/HomePageComponents/AboutCodersCup.jsx
+++ src/components/HomePageComponents/AboutCodersCup.jsx
@@ -1,16 +1,16 @@
-import logo from '../../assets/images/coders-cup-logo.png'
-export default function AboutCodersCup(){
-    return(
-        <div className="text-white bg-[#000017] py-12">
+import logo from '../../assets/images/coders-cup-logo.png';
+
+export default function AboutCodersCup() {
+    return (
+        <div className="text-white bg-[#000017] pt-12">
             <h1 className="bg-[#052F54] text-center text-5xl font-semibold py-6 mb-12">ABOUT CODER'S CUP</h1>
-            <div className="flex flex-row gap-8 justify-center items-center">
-                <p className='w-6/12'>
-                orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
-ment text and icons."orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ment text and icons."orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ment text and icons."orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ment text and icons."orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
-ment text and icons."orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquaem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ment text and icons."orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+            <div className="flex flex-col md:flex-row gap-8 justify-center items-center px-4">
+                <p className='w-full md:w-6/12 text-center md:text-left'>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                 </p>
-                <img className='' src={logo}/>
+                <img className='w-full md:w-4/12' src={logo} alt="Coders Cup Logo" />
             </div>
         </div>
-    )
+    );
 }
