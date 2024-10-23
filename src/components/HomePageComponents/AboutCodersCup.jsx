bdb55ac722fabc5ebaf1cbd0ac655b0ccbaa2836
owaisrafiq05 <owaisrafiq05@gmail.com>
2024-10-23 23:28:33 +0500

    Update 01

--- src/components/HomePageComponents/AboutCodersCup.jsx
+++ src/components/HomePageComponents/AboutCodersCup.jsx
@@ -4,7 +4,7 @@ import logo from '../../assets/images/coders-cup-logo.png';
 export default function AboutCodersCup() {
     return (
         <div className="text-white bg-[#000017] pt-12">
-           <h1 className="text-center text-3xl md:text-7xl font-bold py-6 px-2 mb-12 bg-gradient-to-b from-gray-200 to-gray-300 text-transparent bg-clip-text">
+           <h1 className="text-center text-3xl md:text-7xl font-bold py-6 px-2 bg-gradient-to-b from-gray-200 to-gray-300 text-transparent bg-clip-text">
            ABOUT <span className="text-[#17A0B7]">CODERS CUP</span>
             </h1>
 
