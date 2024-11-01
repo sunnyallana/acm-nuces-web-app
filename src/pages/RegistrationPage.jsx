e303ca536c160f7ee7430e315013644cb27255f1
Sunny Shaban Ali <66526377+sunnyallana@users.noreply.github.com>
2024-11-01 22:23:07 +0500

    Changes

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../components/Register-Now/Header'
 import RegistrationForm from '../components/Register-Now/RegistrationForm'
+import RegistrationClosedNotice from '../components/Register-Now/RegistrationClosedNotice'
 
 const RegistrationPage = () => {
   return (
     <div>
       <Header />
       <RegistrationForm/>
+      {/* <RegistrationClosedNotice/> */}
     </div>
   )
 }
