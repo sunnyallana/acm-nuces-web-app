1574807bf6aff219365ce04e7c9e2f259f1aaad2
Sunny Shaban Ali <66526377+sunnyallana@users.noreply.github.com>
2024-11-12 00:28:52 +0500

    Margin changes

--- src/components/HomePageComponents/AboutCodersCup.jsx
+++ src/components/HomePageComponents/AboutCodersCup.jsx
@@ -44,7 +44,7 @@ export default function AboutCodersCup() {
     }, []);
 
     return (
-        <div className="text-white bg-[#000017] pt-12" ref={sectionRef}>
+        <div className="text-white bg-[#000017] pt-12 mb-28" ref={sectionRef}>
             <h1 className="text-center text-3xl md:text-7xl font-bold py-6 px-2 bg-gradient-to-b from-gray-200 to-gray-300 text-transparent bg-clip-text">
                 ABOUT <span className="text-[#17A0B7]">CODERS CUP</span>
             </h1>
